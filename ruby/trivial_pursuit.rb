bb95552d8f91b5e845c5425889b66e6e272f47a6
Sébastien Charrier <scharrier@gmail.Com>
2014-05-27 13:01:09 +0200

    Step 6 : inheritance and modules

--- ruby/trivial_pursuit.rb
+++ ruby/trivial_pursuit.rb
@@ -1,35 +1,43 @@
-class Card
-  attr_accessor :question, :answer
+module TrivialPursuit
+  class Card
+    attr_accessor :question, :answer
 
-  def initialize question, answer
-    @question = question
-    @answer = answer
+    def initialize question, answer
+      @question = question
+      @answer = answer
+    end
+
+    def correct? guess
+      guess.chomp == @answer
+    end
   end
 
-  def correct? guess
-    guess.chomp == @answer
+  class MultipleAnswersCard < Card
+    def correct? guess
+      @answer.split(",").include? guess.chomp
+    end
+  end
+
+  class Game
+    attr_accessor :cards
+
+    def initialize
+      @cards = []
+    end
+
+    def << card
+      @cards << card
+    end
+
+    def shuffle
+      @cards.shuffle!
+    end
   end
 end
 
-class Game
-  attr_accessor :cards
-
-  def initialize
-    @cards = []
-  end
-
-  def << card
-    @cards << card
-  end
-
-  def shuffle
-    @cards.shuffle!
-  end
-end
-
-game = Game.new
-game << Card.new("When was created Ruby lang ?", "1995")
-game << Card.new("When was created PHP lang ?", "1994")
+game = TrivialPursuit::Game.new
+game << TrivialPursuit::Card.new("When was created Ruby lang ?", "1995")
+game << TrivialPursuit::MultipleAnswersCard.new("Where can you host your code ?", "Github,Gitlab")
 game.shuffle
 
 game.cards.each do |card|
